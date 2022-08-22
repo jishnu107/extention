3938390bf1eae510f076e19ac4e83b0b60a28650
jishnu107 <jleopc1@gmail.com>
2022-08-22 11:19:17 +0530

    added

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('master',views.master_page,name='master'),
-    path('home',views.home_page,name='home'),
+    path('',views.home_page,name='home'),
     path('about',views.about_page,name='about'),
     path('contact',views.contact_page,name='contact'),
     
