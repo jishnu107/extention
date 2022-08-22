0c6f7dbeaf7905037aebdd77d5fbb01e015b0b4a
jishnu107 <jleopc1@gmail.com>
2022-08-22 11:23:07 +0530

    changed to master

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('master',views.master_page,name='master'),
-    path('',views.home_page,name='home'),
+    path('',views.master_page,name='master'),
+    path('home',views.home_page,name='home'),
     path('about',views.about_page,name='about'),
     path('contact',views.contact_page,name='contact'),
     
